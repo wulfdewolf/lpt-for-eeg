393f74c792db149f54855abbc3e876a30c491b28
wulfdewolf <wolf.de.wulf@vub.be>
2021-11-16 14:15:34 +0100

    Added cropped dataset

--- EEGdecoding/datasets/EEGDataset.py
+++ EEGdecoding/datasets/EEGDataset.py
@@ -10,7 +10,6 @@ from EEGdecoding.datasets.dataset import Dataset
 
 
 class EEGDataset(Dataset):
-
     def __init__(self, batch_size, seed, patch_size=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -21,31 +20,40 @@ class EEGDataset(Dataset):
         Loading dataset
         """
         from braindecode.datasets.moabb import MOABBDataset
-        
+
         subject_id = 3
         dataset = MOABBDataset(dataset_name="BNCI2014001", subject_ids=[subject_id])
-        
 
         """
         Preprocessing
         """
         from braindecode.datautil.preprocess import (
-            exponential_moving_standardize, preprocess, Preprocessor)
-        
-        low_cut_hz = 4.  # low cut frequency for filtering
-        high_cut_hz = 38.  # high cut frequency for filtering
+            exponential_moving_standardize,
+            preprocess,
+            Preprocessor,
+        )
+
+        low_cut_hz = 4.0  # low cut frequency for filtering
+        high_cut_hz = 38.0  # high cut frequency for filtering
         # Parameters for exponential moving standardization
         factor_new = 1e-3
         init_block_size = 1000
-        
+
         preprocessors = [
-            Preprocessor('pick_types', eeg=True, meg=False, stim=False),  # Keep EEG sensors
+            Preprocessor(
+                "pick_types", eeg=True, meg=False, stim=False
+            ),  # Keep EEG sensors
             Preprocessor(lambda x: x * 1e6),  # Convert from V to uV
-            Preprocessor('filter', l_freq=low_cut_hz, h_freq=high_cut_hz),  # Bandpass filter
-            Preprocessor(exponential_moving_standardize,  # Exponential moving standardization
-                         factor_new=factor_new, init_block_size=init_block_size)
+            Preprocessor(
+                "filter", l_freq=low_cut_hz, h_freq=high_cut_hz
+            ),  # Bandpass filter
+            Preprocessor(
+                exponential_moving_standardize,  # Exponential moving standardization
+                factor_new=factor_new,
+                init_block_size=init_block_size,
+            ),
         ]
-        
+
         # Transform the data
         preprocess(dataset, preprocessors)
 
@@ -56,9 +64,11 @@ class EEGDataset(Dataset):
         from braindecode.datautil.windowers import create_windows_from_events
 
         trial_start_offset_seconds = -0.5
+
         # Extract sampling frequency, check that they are same in all datasets
-        sfreq = dataset.datasets[0].raw.info['sfreq']
-        assert all([ds.raw.info['sfreq'] == sfreq for ds in dataset.datasets])
+        sfreq = dataset.datasets[0].raw.info["sfreq"]
+        assert all([ds.raw.info["sfreq"] == sfreq for ds in dataset.datasets])
+
         # Calculate the trial start offset in samples.
         trial_start_offset_samples = int(trial_start_offset_seconds * sfreq)
 
@@ -68,21 +78,23 @@ class EEGDataset(Dataset):
             dataset,
             trial_start_offset_samples=trial_start_offset_samples,
             trial_stop_offset_samples=0,
+            window_size_samples=1000,
+            window_stride_samples=2,
+            drop_last_window=False,
             preload=True,
         )
-
 
         """
         Split
         """
-        splitted = windows_dataset.split('session')
-
+        splitted = windows_dataset.split("session")
 
         """
         Set worker seeds for reproducibility
         """
         import torch
         import random
+
         def seed_worker(worker_id):
             torch.manual_seed(seed)
             random.seed(seed)
@@ -95,32 +107,33 @@ class EEGDataset(Dataset):
         Data loader
         """
         self.d_train = DataLoader(
-            splitted['session_T'], 
-            batch_size=batch_size, 
-            drop_last=True, 
+            splitted["session_T"],
+            batch_size=batch_size,
+            drop_last=True,
             shuffle=True,
             worker_init_fn=seed_worker,
             generator=g,
         )
         self.d_test = DataLoader(
-            splitted['session_E'],
-            batch_size=batch_size, 
-            drop_last=True, 
+            splitted["session_E"],
+            batch_size=batch_size,
+            drop_last=True,
             shuffle=True,
             worker_init_fn=seed_worker,
             generator=g,
         )
 
         # Store channels and classes
-        self.n_channels = splitted['session_T'][0][0].shape[0]
-        self.input_window_samples = splitted['session_T'][0][0].shape[1]
+        self.n_channels = splitted["session_T"][0][0].shape[0]
+        self.input_window_samples = splitted["session_T"][0][0].shape[1]
 
         self.train_enum = enumerate(self.d_train)
         self.test_enum = enumerate(self.d_test)
 
-
     def get_batch(self, batch_size=None, train=True):
-        _, (x, y, _) = next(self.train_enum if train else self.test_enum, (None, (None, None, None)))
+        _, (x, y, _) = next(
+            self.train_enum if train else self.test_enum, (None, (None, None, None))
+        )
         if x is None:
             if train:
                 self.train_enum = enumerate(self.d_train)
@@ -129,7 +142,11 @@ class EEGDataset(Dataset):
             _, (x, y, _) = next(self.train_enum if train else self.test_enum)
 
         if self.patch_size is not None:
-            x = rearrange(x, 'batch_size channels (w patch_size) -> batch_size (channels w) patch_size', patch_size=self.patch_size)
+            x = rearrange(
+                x,
+                "batch_size channels (w patch_size) -> batch_size (channels w) patch_size",
+                patch_size=self.patch_size,
+            )
 
         x = x.to(device=self.device)
         y = y.to(device=self.device)
--- EEGdecoding/experiment.py
+++ EEGdecoding/experiment.py
@@ -13,31 +13,28 @@ from EEGdecoding.models.fpt import FPT
 from EEGdecoding.trainer import Trainer
 
 
-def experiment(
-        exp_name,
-        exp_args,
-        **kwargs
-):
+def experiment(exp_name, exp_args, **kwargs):
 
     """
     Preliminary checks
     """
 
     # Must be able to accumulate gradient if batch size is large
-    assert 'batch_size' in kwargs
-    assert kwargs['batch_size'] <= exp_args['gpu_batch_size'] or \
-           kwargs['batch_size'] % exp_args['gpu_batch_size'] == 0
+    assert "batch_size" in kwargs
+    assert (
+        kwargs["batch_size"] <= exp_args["gpu_batch_size"]
+        or kwargs["batch_size"] % exp_args["gpu_batch_size"] == 0
+    )
 
     # Specific threads when running on HPC (https://hpc.vub.be/docs/software/usecases/#pytorch)
-    if exp_args['cluster']:
+    if exp_args["cluster"]:
         torch.set_num_threads(len(os.sched_getaffinity(0)))
         torch.set_num_interop_threads(1)
-
 
     """
     Set seeds for reproducibility
     """
-    seed = kwargs['seed']
+    seed = kwargs["seed"]
     torch.manual_seed(seed)
     random.seed(seed)
     np.random.seed(seed)
@@ -46,28 +43,34 @@ def experiment(
     Create dataset, model, and trainer
     """
 
-    task = kwargs['task']
-    batch_size = kwargs['batch_size']
-    patch_size = kwargs['patch_size']
-    device = exp_args['device']
-    model_type = kwargs['model_type']
+    task = kwargs["task"]
+    batch_size = kwargs["batch_size"]
+    patch_size = kwargs["patch_size"]
+    device = exp_args["device"]
+    model_type = kwargs["model_type"]
 
     return_last_only = True
 
-    if task == 'mnist':
+    if task == "mnist":
         from EEGdecoding.datasets.mnist import MNISTDataset
-        dataset = MNISTDataset(batch_size=batch_size, patch_size=patch_size, device=device)
+
+        dataset = MNISTDataset(
+            batch_size=batch_size, patch_size=patch_size, device=device
+        )
         dataset.get_batch(batch_size)
         input_dim, output_dim = patch_size ** 2, 10
         use_embeddings = False
-    elif task == 'BCI_Competition_IV_2a':
+    elif task == "BCI_Competition_IV_2a":
         from EEGdecoding.datasets.EEGDataset import EEGDataset
-        dataset = EEGDataset(batch_size=batch_size, seed=seed, patch_size=patch_size, device=device)
+
+        dataset = EEGDataset(
+            batch_size=batch_size, seed=seed, patch_size=patch_size, device=device
+        )
         input_dim, output_dim = patch_size, 4
         use_embeddings = False
-        
+
     else:
-        raise NotImplementedError('dataset not implemented')
+        raise NotImplementedError("dataset not implemented")
 
     # Metrics
     ce_loss = torch.nn.CrossEntropyLoss()
@@ -83,61 +86,63 @@ def experiment(
     # Model
     if model_type == "EEG":
         model = ShallowFBCSPNet(
-                    dataset.n_channels,
-                    output_dim,
-                    input_window_samples=dataset.input_window_samples,
-                    final_conv_length='auto',
-                )
+            dataset.n_channels,
+            output_dim,
+            input_window_samples=dataset.input_window_samples,
+            final_conv_length=30,
+        )
     elif model_type == "FPT":
         model = FPT(
             input_dim=input_dim,
             output_dim=output_dim,
-            model_name=kwargs.get('model_name', 'gpt2'),
-            pretrained=kwargs.get('pretrained', True),
+            model_name=kwargs.get("model_name", "gpt2"),
+            pretrained=kwargs.get("pretrained", True),
             return_last_only=return_last_only,
             use_embeddings_for_in=use_embeddings,
-            in_layer_sizes=kwargs.get('in_layer_sizes', None),
-            out_layer_sizes=kwargs.get('out_layer_sizes', None),
-            freeze_trans=kwargs.get('freeze_trans', True),
-            freeze_in=kwargs.get('freeze_in', False),
-            freeze_pos=kwargs.get('freeze_pos', False),
-            freeze_ln=kwargs.get('freeze_ln', False),
-            freeze_attn=kwargs.get('freeze_attn', True),
-            freeze_ff=kwargs.get('freeze_ff', True),
-            freeze_out=kwargs.get('freeze_out', False),
-            dropout=kwargs['dropout'],
-            orth_gain=kwargs['orth_gain'],
+            in_layer_sizes=kwargs.get("in_layer_sizes", None),
+            out_layer_sizes=kwargs.get("out_layer_sizes", None),
+            freeze_trans=kwargs.get("freeze_trans", True),
+            freeze_in=kwargs.get("freeze_in", False),
+            freeze_pos=kwargs.get("freeze_pos", False),
+            freeze_ln=kwargs.get("freeze_ln", False),
+            freeze_attn=kwargs.get("freeze_attn", True),
+            freeze_ff=kwargs.get("freeze_ff", True),
+            freeze_out=kwargs.get("freeze_out", False),
+            dropout=kwargs["dropout"],
+            orth_gain=kwargs["orth_gain"],
         )
     else:
-        raise NotImplementedError('model type not implemented')
+        raise NotImplementedError("model type not implemented")
     model.to(device)
 
     # Trainer
-    gpu_batch_size = exp_args['gpu_batch_size']
+    gpu_batch_size = exp_args["gpu_batch_size"]
     trainer = Trainer(
         model,
         model_type,
         dataset,
         loss_fn=loss_fn,
         accuracy_fn=accuracy_fn,
-        steps_per_epoch=exp_args['steps_per_iter'],
-        test_steps_per_epoch=exp_args['test_steps_per_iter'],
-        learning_rate=kwargs['learning_rate'],
+        steps_per_epoch=exp_args["steps_per_iter"],
+        test_steps_per_epoch=exp_args["test_steps_per_iter"],
+        learning_rate=kwargs["learning_rate"],
         batch_size=gpu_batch_size if batch_size > gpu_batch_size else batch_size,
         eval_batch_size=batch_size,
-        grad_accumulate=batch_size // gpu_batch_size if batch_size > gpu_batch_size else 1,
+        grad_accumulate=batch_size // gpu_batch_size
+        if batch_size > gpu_batch_size
+        else 1,
     )
 
     """
     Set up logging
     """
 
-    log_to_wandb = exp_args['log_to_wandb']
-    save_models = exp_args['save_models']
-    wandb_project = exp_args['wandb_project']
+    log_to_wandb = exp_args["log_to_wandb"]
+    save_models = exp_args["save_models"]
+    wandb_project = exp_args["wandb_project"]
 
     short_name = str(random.randint(int(1e5), int(1e6) - 1))
-    run_name = f'{exp_name}-{task}-{model_type}-{short_name}'
+    run_name = f"{exp_name}-{task}-{model_type}-{short_name}"
 
     if log_to_wandb:
         config = dict(
@@ -147,73 +152,130 @@ def experiment(
             **kwargs,
         )
         wandb.init(
-            name=f'{exp_name}-{model_type}-{short_name}',
-            group=f'{exp_name}-{task}',
+            name=f"{exp_name}-{model_type}-{short_name}",
+            group=f"{exp_name}-{task}",
             project=wandb_project,
             config=config,
         )
         wandb.watch(model)
 
-    for t in range(exp_args['num_iters']):
+    for t in range(exp_args["num_iters"]):
         trainer.train_epoch()
 
-        print('=' * 57)
+        print("=" * 57)
         print(f'| Iteration {" " * 15} | {t+1:25} |')
         for k, v in trainer.diagnostics.items():
-            print(f'| {k:25} | {v:25} |')
+            print(f"| {k:25} | {v:25} |")
 
         if log_to_wandb:
             wandb.log(trainer.diagnostics)
 
-        if save_models and ((t+1) % exp_args['save_models_every'] == 0 or
-                            (t+1) == exp_args['num_iters']):
-            with open(f'models/{run_name}.pt', 'wb') as f:
-                state_dict = dict(model=model.state_dict(), optim=trainer.optim.state_dict())
+        if save_models and (
+            (t + 1) % exp_args["save_models_every"] == 0
+            or (t + 1) == exp_args["num_iters"]
+        ):
+            with open(f"models/{run_name}.pt", "wb") as f:
+                state_dict = dict(
+                    model=model.state_dict(), optim=trainer.optim.state_dict()
+                )
                 torch.save(state_dict, f)
-            print(f'Saved model at {t+1} iters: {run_name}')
+            print(f"Saved model at {t+1} iters: {run_name}")
 
 
 def run_experiment(
-        exp_name,
-        experiment_params,
+    exp_name,
+    experiment_params,
 ):
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--num_iters', '-it', type=int, default=4,
-                        help='Number of iterations for trainer')
-    parser.add_argument('--steps_per_iter', type=int, default=100,
-                        help='Number of gradient steps per iteration')
-    parser.add_argument('--test_steps_per_iter', type=int, default=25,
-                        help='Number of test gradient steps per iteration')
+    parser.add_argument(
+        "--num_iters",
+        "-it",
+        type=int,
+        default=4,
+        help="Number of iterations for trainer",
+    )
+    parser.add_argument(
+        "--steps_per_iter",
+        type=int,
+        default=100,
+        help="Number of gradient steps per iteration",
+    )
+    parser.add_argument(
+        "--test_steps_per_iter",
+        type=int,
+        default=25,
+        help="Number of test gradient steps per iteration",
+    )
 
-    parser.add_argument('--log_to_wandb', '-w', type=bool, default=False,
-                        help='Whether or not to log to Weights and Biases')
-    parser.add_argument('--note', '-n', type=str, default='',
-                        help='An optional note to be logged to W&B')
-    parser.add_argument('--wandb_project', type=str, default='fpt-for-eeg',
-                        help='Project name for W&B')
-    parser.add_argument('--include_date', type=bool, default=True,
-                        help='Whether to include date in run name')
+    parser.add_argument(
+        "--log_to_wandb",
+        "-w",
+        type=bool,
+        default=False,
+        help="Whether or not to log to Weights and Biases",
+    )
+    parser.add_argument(
+        "--note",
+        "-n",
+        type=str,
+        default="",
+        help="An optional note to be logged to W&B",
+    )
+    parser.add_argument(
+        "--wandb_project", type=str, default="fpt-for-eeg", help="Project name for W&B"
+    )
+    parser.add_argument(
+        "--include_date",
+        type=bool,
+        default=True,
+        help="Whether to include date in run name",
+    )
 
-    parser.add_argument('--save_models', '-s', type=bool, default=False,
-                        help='Whether or not to save the model files locally')
-    parser.add_argument('--save_models_every', '-int', type=int, default=25,
-                        help='How often to save models locally')
+    parser.add_argument(
+        "--save_models",
+        "-s",
+        type=bool,
+        default=False,
+        help="Whether or not to save the model files locally",
+    )
+    parser.add_argument(
+        "--save_models_every",
+        "-int",
+        type=int,
+        default=25,
+        help="How often to save models locally",
+    )
 
-    parser.add_argument('--cluster', '-c', type=bool, default=False,
-                        help='Whether or not the experiment is ran on the HPC cluster')
-    parser.add_argument('--device', '-d', type=str, default='cuda',
-                        help='Which device for Pytorch to use')
-    parser.add_argument('--gpu_batch_size', '-gbs', type=int, default=16,
-                        help='Max batch size to put on GPU (used for gradient accumulation)')
+    parser.add_argument(
+        "--cluster",
+        "-c",
+        type=bool,
+        default=False,
+        help="Whether or not the experiment is ran on the HPC cluster",
+    )
+    parser.add_argument(
+        "--device",
+        "-d",
+        type=str,
+        default="cuda",
+        help="Which device for Pytorch to use",
+    )
+    parser.add_argument(
+        "--gpu_batch_size",
+        "-gbs",
+        type=int,
+        default=16,
+        help="Max batch size to put on GPU (used for gradient accumulation)",
+    )
 
     exp_args = parser.parse_args(sys.argv[1:])
 
     if exp_args.include_date:
-        timestamp = datetime.now().strftime('%m-%d')
-        exp_name = f'{timestamp}-{exp_name}'
+        timestamp = datetime.now().strftime("%m-%d")
+        exp_name = f"{timestamp}-{exp_name}"
 
-    experiment_params['exp_name'] = exp_name
-    experiment_params['exp_args'] = vars(exp_args)
+    experiment_params["exp_name"] = exp_name
+    experiment_params["exp_args"] = vars(exp_args)
 
     experiment(xp_name=exp_name, **experiment_params)
