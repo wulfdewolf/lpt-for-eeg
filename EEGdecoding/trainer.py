124e06cc38e1c7a2a816e8940c4047f5023c83be
wulfdewolf <wolf.de.wulf@vub.be>
2021-11-16 14:47:37 +0100

    removed batch_size param from get_batch in trainer, batch_size is coded into the dataset

--- EEGdecoding/trainer.py
+++ EEGdecoding/trainer.py
@@ -5,20 +5,19 @@ import time
 
 
 class Trainer:
-
     def __init__(
-            self,
-            model,
-            model_type,
-            dataset,
-            loss_fn,
-            accuracy_fn=None,
-            steps_per_epoch=100,
-            test_steps_per_epoch=20,
-            learning_rate=1e-3,
-            batch_size=2,
-            eval_batch_size=8,
-            grad_accumulate=1,
+        self,
+        model,
+        model_type,
+        dataset,
+        loss_fn,
+        accuracy_fn=None,
+        steps_per_epoch=100,
+        test_steps_per_epoch=20,
+        learning_rate=1e-3,
+        batch_size=2,
+        eval_batch_size=8,
+        grad_accumulate=1,
     ):
         self.model = model
         self.model_type = model_type
@@ -33,18 +32,20 @@ class Trainer:
 
         self.optim = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-        self.diagnostics = {'Gradient Steps': 0}
+        self.diagnostics = {"Gradient Steps": 0}
 
     def get_loss(self, x, y, return_acc=False):
         out = self.model(x)
 
-        if self.model_type == 'EEG': 
-            out = out[:,None,:]         # braindecode uses a different format, extra nested level is needed around the predicted class probs
+        if self.model_type == "EEG":
+            out = out[
+                :, None, :
+            ]  # braindecode uses a different format, extra nested level is needed around the predicted class probs
 
         loss = self.loss_fn(out, y, x=x)
         if return_acc:
             if self.acc_fn is None:
-                raise NotImplementedError('accuracy function not specified')
+                raise NotImplementedError("accuracy function not specified")
             accs = self.acc_fn(
                 out.detach().cpu().numpy(),
                 y.detach().cpu().numpy(),
@@ -56,31 +57,31 @@ class Trainer:
     def train_epoch(self, test_steps=None):
         self.dataset.start_epoch()
 
-        train_losses, tr_accuracy = [], 0.
+        train_losses, tr_accuracy = [], 0.0
         self.model.train()
         start_train_time = time.time()
         for _ in tqdm(range(self.steps_per_epoch)):
             step_loss = 0
             for _ in range(self.grad_accumulate):
-                x, y = self.dataset.get_batch(self.batch_size, train=True)
+                x, y = self.dataset.get_batch(train=True)
                 loss, acc = self.get_loss(x, y, return_acc=True)
                 loss = loss / self.grad_accumulate
                 loss.backward()
                 step_loss += loss.detach().cpu().item()
                 tr_accuracy += acc
 
-            torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1.)
+            torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1.0)
             self.optim.step()
             self.optim.zero_grad()
 
-            self.diagnostics['Gradient Steps'] += 1
+            self.diagnostics["Gradient Steps"] += 1
 
             train_losses.append(step_loss)
         end_train_time = time.time()
 
         test_steps = self.test_steps_per_epoch if test_steps is None else test_steps
 
-        test_loss, accuracy = 0., 0.
+        test_loss, accuracy = 0.0, 0.0
         self.model.eval()
         start_test_time = time.time()
         with torch.no_grad():
@@ -91,11 +92,15 @@ class Trainer:
                 accuracy += acc / test_steps
         end_test_time = time.time()
 
-        self.diagnostics['Average Train Loss'] = sum(train_losses) / self.steps_per_epoch
-        self.diagnostics['Start Train Loss'] = train_losses[0]
-        self.diagnostics['Final Train Loss'] = train_losses[-1]
-        self.diagnostics['Test Loss'] = test_loss
-        self.diagnostics['Test Accuracy'] = accuracy
-        self.diagnostics['Train Accuracy'] = tr_accuracy / (self.steps_per_epoch * self.grad_accumulate)
-        self.diagnostics['Time Training'] = end_train_time - start_train_time
-        self.diagnostics['Time Testing'] = end_test_time - start_test_time
+        self.diagnostics["Average Train Loss"] = (
+            sum(train_losses) / self.steps_per_epoch
+        )
+        self.diagnostics["Start Train Loss"] = train_losses[0]
+        self.diagnostics["Final Train Loss"] = train_losses[-1]
+        self.diagnostics["Test Loss"] = test_loss
+        self.diagnostics["Test Accuracy"] = accuracy
+        self.diagnostics["Train Accuracy"] = tr_accuracy / (
+            self.steps_per_epoch * self.grad_accumulate
+        )
+        self.diagnostics["Time Training"] = end_train_time - start_train_time
+        self.diagnostics["Time Testing"] = end_test_time - start_test_time
