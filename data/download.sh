5d1b844df7c64a553155c22885c4d4e766d25db4
Wolf <wolf2wulf@gmail.com>
2022-03-09 11:27:47 +0100

    added evaluation data and processing code for it, as explained by BENDR paper author + cleanup of thesis doc stuff, separate overleaf for this

--- data/download.sh
+++ data/download.sh
@@ -1,23 +1,86 @@
 #!/bin/bash
 
 # Make directory for raws
-#mkdir data/competition
-#mkdir data/competition/raw
-#
-## Download
-#wget -P data/competition/raw/ "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A01T.mat"
-#wget -P data/competition/raw/ "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A02T.mat"
-#wget -P data/competition/raw/ "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A03T.mat"
-#wget -P data/competition/raw/ "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A04T.mat"
-#wget -P data/competition/raw/ "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A05T.mat"
-#wget -P data/competition/raw/ "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A06T.mat"
-#wget -P data/competition/raw/ "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A07T.mat"
-#wget -P data/competition/raw/ "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A08T.mat"
-#wget -P data/competition/raw/ "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A09T.mat"
+if [ ! -d data/competition ]; then
+    mkdir data/competition
+fi
+if [ ! -d data/competition/raw ]; then
+    mkdir data/competition/raw
+fi
+
+# Download training data
+if [ ! -d data/competition/raw/training ]; then
+    mkdir data/competition/raw/training
+fi
+if ! test -f data/competition/raw/training/A01T.mat; then
+    wget -P data/competition/raw/training "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A01T.mat"
+fi
+if ! test -f data/competition/raw/training/A02T.mat; then
+    wget -P data/competition/raw/training "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A02T.mat"
+fi
+if ! test -f data/competition/raw/training/A03T.mat; then
+    wget -P data/competition/raw/training "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A03T.mat"
+fi
+if ! test -f data/competition/raw/training/A04T.mat; then
+    wget -P data/competition/raw/training "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A04T.mat"
+fi
+if ! test -f data/competition/raw/training/A05T.mat; then
+    wget -P data/competition/raw/training "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A05T.mat"
+fi
+if ! test -f data/competition/raw/training/A06T.mat; then
+    wget -P data/competition/raw/training "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A06T.mat"
+fi
+if ! test -f data/competition/raw/training/A07T.mat; then
+    wget -P data/competition/raw/training "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A07T.mat"
+fi
+if ! test -f data/competition/raw/training/A08T.mat; then
+    wget -P data/competition/raw/training "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A08T.mat"
+fi
+if ! test -f data/competition/raw/training/A09T.mat; then
+    wget -P data/competition/raw/training "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A09T.mat"
+fi
+
+# Download evaluation data
+if [ ! -d data/competition/raw/evaluation ]; then
+    mkdir data/competition/raw/evaluation
+fi
+if ! test -f data/competition/raw/evaluation/A01E.mat; then
+    wget -P data/competition/raw/evaluation "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A01E.mat"
+fi
+if ! test -f data/competition/raw/evaluation/A02E.mat; then
+    wget -P data/competition/raw/evaluation "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A02E.mat"
+fi
+if ! test -f data/competition/raw/evaluation/A03E.mat; then
+    wget -P data/competition/raw/evaluation "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A03E.mat"
+fi
+if ! test -f data/competition/raw/evaluation/A04E.mat; then
+    wget -P data/competition/raw/evaluation "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A04E.mat"
+fi
+if ! test -f data/competition/raw/evaluation/A05E.mat; then
+    wget -P data/competition/raw/evaluation "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A05E.mat"
+fi
+if ! test -f data/competition/raw/evaluation/A06E.mat; then
+    wget -P data/competition/raw/evaluation "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A06E.mat"
+fi
+if ! test -f data/competition/raw/evaluation/A07E.mat; then
+    wget -P data/competition/raw/evaluation "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A07E.mat"
+fi
+if ! test -f data/competition/raw/evaluation/A08E.mat; then
+    wget -P data/competition/raw/evaluation "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A08E.mat"
+fi
+if ! test -f data/competition/raw/evaluation/A09E.mat; then
+    wget -P data/competition/raw/evaluation "http://bnci-horizon-2020.eu/database/data-sets/001-2014/A09E.mat"
+fi
 
 # Make directory for weights
-mkdir data/weights
+if [ ! -d data/weights ]; then
+    mkdir data/weights
+fi
 
 # Download
-wget -P data/weights "https://github.com/SPOClab-ca/BENDR/releases/download/v0.1-alpha/contextualizer.pt"
-wget -P data/weights "https://github.com/SPOClab-ca/BENDR/releases/download/v0.1-alpha/encoder.pt"
+if ! test -f data/weights/contextualizer.pt; then
+    wget -P data/weights "https://github.com/SPOClab-ca/BENDR/releases/download/v0.1-alpha/contextualizer.pt"
+fi
+if ! test -f data/weights/encoder.pt; then
+    wget -P data/weights "https://github.com/SPOClab-ca/BENDR/releases/download/v0.1-alpha/encoder.pt"
+fi
--- data/process.py
+++ data/process.py
@@ -39,59 +39,67 @@ ch_types = ["eeg"] * 22 + ["eog"] * 3 + ["stim"]
 # Create processed folder
 os.mkdir("data/competition/processed")
 
-# Per subject
-for subject_id, subject_file in enumerate(os.listdir("data/competition/raw")):
+# Per type
+for type, folder in zip(
+    ["T", "E"], ["data/competition/raw/training", "data/competition/raw/evaluation"]
+):
+    # Per subject
+    for subject_id, subject_file in enumerate(os.listdir(folder)):
 
-    # Create subject folder
-    os.mkdir("data/competition/processed/" + str(subject_id))
+        # Create subject folder
+        if not os.path.exists("data/competition/processed/" + str(subject_id)):
+            os.mkdir("data/competition/processed/" + str(subject_id))
 
-    # Load raw file
-    data = loadmat(
-        "data/competition/raw/" + str(subject_file),
-        struct_as_record=False,
-        squeeze_me=True,
-    )
-
-    # Per session
-    for run_id, run in enumerate(data["data"][3:]):
-
-        """Convert one run to raw."""
-        event_id = {}
-        n_chan = run.X.shape[1]
-        montage = mne.channels.make_standard_montage("standard_1005")
-        eeg_data = 1e-6 * run.X
-        sfreq = run.fs
-
-        # Stim channel
-        trigger = np.zeros((len(eeg_data), 1))
-        trigger[run.trial - 1, 0] = run.y
-        eeg_data = np.c_[eeg_data, trigger]
-
-        # Events
-        event_id = {ev: (ii + 1) for ii, ev in enumerate(run.classes)}
-        event_ids = {
-            class_value: class_label
-            for class_value, class_label in zip(np.unique(run.y), run.classes)
-        }
-        events = np.column_stack((run.trial, np.zeros(len(run.y), dtype=int), run.y))
-
-        # Annotations
-        annotations = mne.annotations_from_events(
-            events, event_desc=event_ids, sfreq=sfreq
+        # Load raw file
+        data = loadmat(
+            folder + "/" + str(subject_file),
+            struct_as_record=False,
+            squeeze_me=True,
         )
 
-        # Create MNE structures
-        info = mne.create_info(ch_names=ch_names, ch_types=ch_types, sfreq=sfreq)
-        raw = mne.io.RawArray(data=eeg_data.T, info=info)
-        raw.set_montage(montage)
-        # raw.set_annotations(annotations)
-        raw.add_events(events, stim_channel="stim")
+        # Per session
+        for run_id, run in enumerate(data["data"][3:]):
 
-        # Save
-        raw.save(
-            "data/competition/processed/"
-            + str(subject_id)
-            + "/"
-            + str(run_id)
-            + "-raw.fif"
-        )
+            """Convert one run to raw."""
+            event_id = {}
+            n_chan = run.X.shape[1]
+            montage = mne.channels.make_standard_montage("standard_1005")
+            eeg_data = 1e-6 * run.X
+            sfreq = run.fs
+
+            # Stim channel
+            trigger = np.zeros((len(eeg_data), 1))
+            trigger[run.trial - 1, 0] = run.y
+            eeg_data = np.c_[eeg_data, trigger]
+
+            # Events
+            event_id = {ev: (ii + 1) for ii, ev in enumerate(run.classes)}
+            event_ids = {
+                class_value: class_label
+                for class_value, class_label in zip(np.unique(run.y), run.classes)
+            }
+            events = np.column_stack(
+                (run.trial, np.zeros(len(run.y), dtype=int), run.y)
+            )
+
+            # Annotations
+            annotations = mne.annotations_from_events(
+                events, event_desc=event_ids, sfreq=sfreq
+            )
+
+            # Create MNE structures
+            info = mne.create_info(ch_names=ch_names, ch_types=ch_types, sfreq=sfreq)
+            raw = mne.io.RawArray(data=eeg_data.T, info=info)
+            raw.set_montage(montage)
+            # raw.set_annotations(annotations)
+            raw.add_events(events, stim_channel="stim")
+
+            # Save
+            raw.save(
+                "data/competition/processed/"
+                + str(subject_id)
+                + "/"
+                + str(run_id)
+                + type
+                + "-raw.fif"
+            )
